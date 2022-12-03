7cb8b931d26bf18a9a50a4a9575832d184cabb57
gillaryb <gilarybacnis@gmail.com>
2022-12-03 16:00:47 +0900

    moved weather info to different component

--- src/MainWeather.js
+++ src/MainWeather.js
@@ -1,33 +1,15 @@
-import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
-import axios from "axios";
 import "./MainWeather.css";
 
 export default function MainWeather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-
-  function displayResponse(response) {
-    setWeatherData({
-      ready: true,
-      temperature: response.data.main.temp,
-      date: new Date (response.data.dt * 1000),
-      city: response.data.name,
-      description: response.data.weather[0].description,
-      max: response.data.main.temp_max,
-      min: response.data.main.temp_min,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-    });
-  }
-
-  if (weatherData.ready) {
     return (
-      <div className=" MainWeather shadow">
-        <div className="date-today"> <FormattedDate date={weatherData.date} />
+      <div className=" MainWeather shadow rounded">
+        <div className="date-today"> <FormattedDate date={props.data.date} />
         </div>
-        <h1 className="city-name">{weatherData.city}</h1>
-        <img src={weatherData.icon} alt={weatherData.description} width="135px" />
+        <h1 className="city-name">{props.data.city}</h1>
+        <img src={props.data.icon} alt={props.data.description} width="135px" />
         <div className="weather-temperature p-2">
-          <strong>{Math.round(weatherData.temperature)}</strong>
+          <strong>{Math.round(props.data.temperature)}</strong>
           <span className="celFarh">
             <a href="null" className="celcius">
               ˚C{" "}
@@ -39,21 +21,14 @@ export default function MainWeather(props) {
           </span>
         </div>
         <ul className="weather">
-          <li className="today">{weatherData.description}</li>
+          <li className="today">{props.data.description}</li>
           <li className="max-min">
             <strong>Max:</strong>
-            <span className="temp-today"> {Math.round(weatherData.max)}˚</span> {" "}
+            <span className="temp-today"> {Math.round(props.data.max)}˚</span> {" "}
             <strong>Min:</strong>
-            <span> {Math.round(weatherData.min)}˚</span>
+            <span> {Math.round(props.data.min)}˚</span>
           </li>
         </ul>
       </div>
     );
-  } else {
-    const apiKey = "f0553e70ab5eb275ae36ae41c6ace9b0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayResponse);
-
-    return "Loading...";
-  }
 }
--- src/MenuBar.js
+++ src/MenuBar.js
@@ -1,32 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import MainWeather from "./MainWeather";
+import axios from "axios";
 import "./MenuBar.css";
 
-export default function MenuBar() {
-  return (
-    <div className="Menubar pt-4">
-      <form>
-        <div className="row">
-          <div className="col-8">
-            <input
-              type="search"
-              placeholder="Search for your city"
-              size="40"
-              autofocus="on"
-              className="search-field shadow-sm"
-            />
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary search-input "
-            />
-            <input
-              type="submit"
-              value="Current Location"
-              className="btn btn-outline-primary search-button"
-            />
-        </div>
-        </div>
-      </form>
-    </div>
-  );
+export default function MenuBar(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function displayResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      date: new Date(response.data.dt * 1000),
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      max: response.data.main.temp_max,
+      min: response.data.main.temp_min,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event){
+    event.preventDefault();
+    // search();
+  }
+
+  function handleCityChange(event){
+    setCity(event.target.value);
+  }
+  if (weatherData.ready){
+    return (
+      <div className="Menubar pt-4">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="search"
+                placeholder="Search for your city"
+                size="40"
+                autoFocus="on"
+                className="search-field shadow-sm"
+                onChange={handleCityChange}
+              />
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary search-input "
+              />
+              <input
+                type="submit"
+                value="Current Location"
+                className="btn btn-outline-primary search-button"
+              />
+            </div>
+          </div>
+        </form>
+         <MainWeather data={weatherData} />
+      </div>
+    
+    );
+ } else {
+    const apiKey = "f0553e70ab5eb275ae36ae41c6ace9b0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayResponse);
+
+    return "Loading...";
+  }
+
 }
